2e567ad447d0ead58c760240cdaf76acddeeafb5
Umesh Gamit <umesh.gamit@hotmail.com>
2024-04-28 13:42:08 +0530

    fixed issue

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
-import { Spotlight } from "@/components/ui/spotlight";
 import { Button } from "./ui/moving-border";
+import { Spotlight } from "./ui/spotlight";
 
 function HeroSection() {
   return (
